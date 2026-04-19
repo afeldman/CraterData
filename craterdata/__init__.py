2dd4ba3f89dda6688e19fef32ba1c1d4d8385fb3
Anton Feldmann <anton.feldmann@lynq.tech>
2026-04-19 17:48:14 +0200

    feat: add SyntheticCraterDataset for monte-carlo-sim HDF5 shards

--- craterdata/__init__.py
+++ craterdata/__init__.py
@@ -1,3 +1,8 @@
 __author__ = "Anton Feldmann"
 __email__ = "anton.feldmann@gmail.com"
-__version__ = (0, 1, 3)
+__version__ = (0, 2, 0)
+
+from craterdata.mooncraterdataset import MoonCraterDataset
+from craterdata.synthetic_dataset import SyntheticCraterDataset
+
+__all__ = ["MoonCraterDataset", "SyntheticCraterDataset"]
